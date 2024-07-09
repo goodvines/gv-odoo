cd31aa8fbc90c194fb490e73207a0e12c460c53a
aleemcahn <akhan@immap.org>
2024-07-09 13:47:39 +0500

    [Script]

---
+++ goodvines_customizations/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Goodvines Specific Customizations',
+    'summary': '',
+    'version': '17.0',
+    'author': 'CaanTech',
+    'category': '',
+    'license': 'LGPL-3',
+    'website': "",
+    'description': """ """,
+    'depends': [
+        'base_vat',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'auto_install': True,
+}
